4cefdf52692d4e360e373481ebc6d288ec56d4c5
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-01 08:18:21 +0300

    query selector

---
+++ app.js
@@ -0,0 +1,11 @@
+// to select items use the css selectors as strins inside the query
+//selector method
+//select the first paragraph in the document using paragraph selector
+//this selects the first p tag in the document
+// const para = document.querySelector("p");
+
+
+//select last p tag using css class selector
+const para = document.querySelector('div.error');
+console.log(para);
+
