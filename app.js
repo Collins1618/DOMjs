bdf22c1794131711bee9e574e0bbc1d698a18340
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-01 09:22:35 +0300

    cycle through multiple elements changing the inner text using foreach"

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const para = document.querySelector('p'); //first paragraph on the page
 
 //update inner text on the first paragraph\
 //override the older text completely
-para.innerText = "ninjas are awesome!"; 
+// para.innerText = "ninjas are awesome!"; 
 
 //append to the end of the paragraph
 // para.innerText += " ninjas are awesome!"
+
+//change the text of several items at once
+const paras = document.querySelectorAll('p'); //grab all p tags
+
+//cycle through a node list using foreach
+paras.forEach(para => {
+    console.log(para.innerText);
+    para.innerText += " new text";
+})
 
 
