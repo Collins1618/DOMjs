9f6e99850372b0a4827b7577e68c7fe7f35df1e7
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-01 11:02:58 +0300

    get and set element attributes

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
-//Adding and changing page content
-const para = document.querySelector('p'); //first paragraph on the page
+//changing attributes
+const link  = document.querySelector('a');
 
-// console.log(para.innerText)
+console.log(link.getAttribute('href'))
 
-//update inner text on the first paragraph\
-//override the older text completely
-// para.innerText = "ninjas are awesome!"; 
+link.setAttribute('href', 'https://wwww.thenetninja.co.uk');
+link.innerText = 'The net ninja website';
 
-//append to the end of the paragraph
-// para.innerText += " ninjas are awesome!"
+const msg = document.querySelector('p');
+console.log(msg.getAttribute('class'));
+msg.setAttribute('class', 'success');
 
-//change the text of several items at once
-const paras = document.querySelectorAll('p'); //grab all p tags
-
-//cycle through a node list using foreach
-// paras.forEach(para => {
-//     console.log(para.innerText);
-//     para.innerText += " new text";
-// })
-
-//change the html on a page
-const content = document.querySelector('.content');
-
-// console.log(content.innerHTML);
-
-// content.innerHTML += '<h2>THIS IS A NEW H2</h2>';// += APPEND, = OVERWRITE
-
-const people = ['mario', 'luigi', 'yoshi'];
-people.forEach(person => {
-    let ul = document.querySelector('ul');
-    ul.innerHTML += `<li>${person}</li>`;
-});
+msg.setAttribute('style', 'color:green');
