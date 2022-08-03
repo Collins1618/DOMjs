bdbf4644bf5a3f501f629d94d06d8a1e967d1392
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-03 12:17:28 +0300

    making use of the parent child relationship

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-//Challenge
-const paras = document.querySelectorAll('p');
-paras.forEach(para => {
+const article = document.querySelector('article');
+console.log(article.children);
 
-    //use text content instaed in order to get even the text that is hidden
-    //thisis because inner text won't capture hidden text
-    if(para.textContent.includes('error')){
-        para.classList.add('error');
-    }
-    else if(para.textContent.includes('success')){
-        para.classList.add('success');
-    }
+Array.from(article.children).forEach(child => {
+    child.classList.add('article-element');
 })
-
-//toggling classes
-const title = document.querySelector('.title');
-
-title.classList.toggle('test');//add test class
-title.classList.toggle('test');//remove test class
