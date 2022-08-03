1e33cc1b6a780870c17d302a11cf46677f9184e8
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-03 12:24:24 +0300

    get an elements parent using parentElement and also chaining it to get gparents

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const article = document.querySelector('article');
-console.log(article.children);
+// console.log(article.children);
 
-Array.from(article.children).forEach(child => {
-    child.classList.add('article-element');
-})
+// Array.from(article.children).forEach(child => {
+//     child.classList.add('article-element');
+// })
+
+const title = document.querySelector('h2');
+console.log(title.parentElement);
+console.log(title.parentElement.parentElement); //we can chain
+//these thigns together
+
+
