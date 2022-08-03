489f44db1c461552d9324a6fef35cad69d50f159
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-03 09:09:14 +0300

    replace innerText with textContent in order to capture even the hidden texts

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //Challenge
 const paras = document.querySelectorAll('p');
 paras.forEach(para => {
-    if(para.innerText.includes('error')){
+
+    //use text content instaed in order to get even the text that is hidden
+    //thisis because inner text won't capture hidden text
+    if(para.textContent.includes('error')){
         para.classList.add('error');
     }
-    else if(para.innerText.includes('success')){
+    else if(para.textContent.includes('success')){
         para.classList.add('success');
     }
 })
