affe871489577b66e03a14583e38b2c01ab49ad8
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-03 08:27:25 +0300

    adding and removing css styles

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 //changing attributes
-const link  = document.querySelector('a');
+// const link  = document.querySelector('a');
 
-console.log(link.getAttribute('href'))
+// console.log(link.getAttribute('href'))
 
-link.setAttribute('href', 'https://wwww.thenetninja.co.uk');
-link.innerText = 'The net ninja website';
+// link.setAttribute('href', 'https://wwww.thenetninja.co.uk');
+// link.innerText = 'The net ninja website';
 
-const msg = document.querySelector('p');
-console.log(msg.getAttribute('class'));
-msg.setAttribute('class', 'success');
+// const msg = document.querySelector('p');
+// console.log(msg.getAttribute('class'));
+// msg.setAttribute('class', 'success');
 
-msg.setAttribute('style', 'color:green');
+// msg.setAttribute('style', 'color:green');
+
+const title = document.querySelector('h1');
+
+console.log(title.style);
+console.log(title.style.color)
+
+//adding styles to Css
+title.style.margin = '50px';
+title.style.color = 'crimson';
+title.style.fontSize = '60px';
+
+//deleting a style
+title.style.margin ='';
