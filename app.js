0082ddfe96ac3d7bcc6f92dc9f622236d37ade8f
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-03 13:31:31 +0300

    adding event listeners to items, and items in nodelist

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-//add an event listener using the addEventListener methods
-button.addEventListener('click', () => {
-    console.log("you clicked me");
-});
+// //add an event listener using the addEventListener methods
+// button.addEventListener('click', () => {
+//     console.log("you clicked me");
+// });
+
+const items = document.querySelectorAll('li'); //nodelist of li tags
+
+items.forEach(item => {
+    item.addEventListener('click', e => {
+        // console.log('item clicked');
+        // console.log(e);
+        // console.log(e.target);
+        // console.log(item); // we can access the element using the item
+        //becaus we have access to items when we cycling through itmes
+        e.target.style.textDecoration = 'line-through';
+    });
+})
