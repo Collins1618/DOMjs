c763370c983884e97b97875a69ef5d75556f2798
Collins Kariuki <collinsmunkary@gmail.com>
2022-08-04 08:07:50 +0300

    remove elements from webpage

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
 //     console.log("you clicked me");
 // });
 
+const ul = document.querySelector('ul');
+// ul.remove();
+
 const items = document.querySelectorAll('li'); //nodelist of li tags
 
 items.forEach(item => {
@@ -14,6 +17,7 @@ items.forEach(item => {
         // console.log(e.target);
         // console.log(item); // we can access the element using the item
         //becaus we have access to items when we cycling through itmes
-        e.target.style.textDecoration = 'line-through';
+        // e.target.style.textDecoration = 'line-through';
+        e.target.remove(); //remove clicked element from webpage
     });
 })
